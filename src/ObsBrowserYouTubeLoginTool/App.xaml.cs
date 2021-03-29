2377a5ee39eb4ab9538a91e44e9cb6f0cc80181d
Kanna <hamster.happiness+gitkraken@gmail.com>
2021-03-29 11:21:42 +0900

    プロジェクト ファイルを追加します。

---
+++ src/ObsBrowserYouTubeLoginTool/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using ObsBrowserYouTubeLoginTool.Views;
+using Prism.Ioc;
+using System.Windows;
+
+namespace ObsBrowserYouTubeLoginTool
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
